32597039ff9d7e6042f1d77e8b4fd9492fb5fdda
bonggyuKim <50454395+bonggyuKim@users.noreply.github.com>
2022-06-16 09:59:33 +0900

    multispinkick

--- DeepMimicUnreal_multi.py
+++ DeepMimicUnreal_multi.py
@@ -18,13 +18,14 @@ def build_arg_parser(filename):
 
 def convert_data_to_float(data):
     global agentNum
+    global agentBehavior
     dataArray = data.split(" ")
     #dataArray.pop(454)
     #print(dataArray)
-    dataArray.pop(0)
-    dataArray.pop(0)
-    agentNum.append(int(dataArray.pop(0)))
-    agentNum.append(int(dataArray.pop(226)))
+    agentBehavior.append(int(dataArray.pop(0)))
+    agentBehavior.append(int(dataArray.pop(0)))
+    agentNum.append(dataArray.pop(0))
+    agentNum.append(dataArray.pop(226))
     floatArray = []
     stateSize = 226
 
@@ -49,17 +50,18 @@ PORT = 3000
 data_size = 8000
 
 agentNum = []
-
+agentBehavior = []
 
 def main():
     global world
     global env
     global agentNum
-
-    policy = ['args/run_amp_humanoid3d_walk_args.txt', 'args/run_amp_humanoid3d_walk_args.txt']
-    arg_parser1 = build_arg_parser(policy[0])
-    arg_parser2 = build_arg_parser(policy[1])
-    arg_parser = [arg_parser1, arg_parser2]
+    global agentBehavior
+    policy = ['args/run_amp_humanoid3d_walk_args.txt', 'args/run_amp_humanoid3d_walk_args.txt', 'args/run_amp_humanoid3d_spinkick_args.txt']
+    arg_parser = []
+    for i in policy:
+        arg = build_arg_parser(i)
+        arg_parser.append(arg)
     env = UnrealEnv(arg_parser, enable_draw=True)
     world = UnrealRL(env, arg_parser)
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -67,18 +69,18 @@ def main():
     print("Socket Ready")
     #try:
     while 1:
+        agentBehavior = []
         client, address = server.recvfrom(30000)
         action = []
         data = client.decode("utf-8")
         states = convert_data_to_float(data)
+        world.env.store_state(agentBehavior[0], states[0:226])
+        world.update(agentBehavior[0])
+        action.extend(world.env.set_unreal_action(0))
 
-        world.env.store_state(agentNum[0], states[0:226])
-        world.update(agentNum[0])
-        action.extend(world.env.set_unreal_action(agentNum[0]))
-
-        world.env.store_state(agentNum[1], states[226:])
-        world.update(agentNum[1])
-        action.extend(world.env.set_unreal_action(agentNum[1]))
+        world.env.store_state(agentBehavior[1], states[226:])
+        world.update(agentBehavior[1])
+        action.extend(world.env.set_unreal_action(1))
 
         action = convert_data_to_string(action)
         server.sendto(action.encode(), address)
